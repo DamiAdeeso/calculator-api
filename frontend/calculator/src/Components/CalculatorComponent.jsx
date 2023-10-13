2b739eab1c1c674d66e7672cd086ee97c4d085cf
DamiAdeeso <damilolaadeeso@gmail.com>
2023-10-13 23:53:37 +0100

    working on display logic

--- frontend/calculator/src/Components/CalculatorComponent.jsx
+++ frontend/calculator/src/Components/CalculatorComponent.jsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
-import Container from 'react-bootstrap/esm/Container'; 
+import Container from 'react-bootstrap/esm/Container';
 import axios from 'axios';
 
 function Example() {
@@ -12,13 +12,14 @@ function Example() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const [operand1,setOperand1] =useState("");
-  const [operand2,setOperand2] =useState("");
-  const [operator,setOperator] =useState("");
-  const [isOperatorOn,setIsOperatorOn] =useState(false);
+  const [operand1, setOperand1] = useState("");
+  const [operand2, setOperand2] = useState("");
+  const [operator, setOperator] = useState("");
+  const [countAfterOperator, setCountAfterOperator] = useState();
+  const [isOperatorOn, setIsOperatorOn] = useState(false);
   // let isOperatorOn = false;
-  const [calcField, setCalcField] = useState(0);
- 
+  const [calcField, setCalcField] = useState("");
+
 
   //reset function is for CE button to clear memory and field of the calculator
   const reset = () => {
@@ -26,127 +27,138 @@ function Example() {
     setOperand1(0);
     updateOperator("");
     setIsOperatorOn(true);
-    isOperatorOn=false;
+    isOperatorOn = false;
   }
 
   const onCalculate = async (operator) => {
+    console.log("in calculate" + operand1 + " " + operator + " " + operand2)
     //this function both operands a operator to the backend and displays the data sent back to field
     const result = await axios.post(`/api/${operator}`, {
-      operand1: operand1,
-      operand2: operand2
+      operand1: parseInt(operand1),
+      operand2: parseInt(operand2)
     });
 
     setCalcField(result.data);
+    setOperand1(result.data.toString());
+    setOperand2("");
+    setOperator("");
+    setIsOperatorOn(false);
   }
 
-  const updateOperand =  (operand) =>{
-    setCalcField(operand);
+  const updateOperand = (operand) => {
+    setCalcField(calcField + operand);
+   
+    if (!isOperatorOn) { 
+      setOperand1(operand1 + operand) 
+    } else {
+      setCountAfterOperator( +1);console.log("here" +countAfterOperator)
+      if (countAfterOperator == 1) { setCalcField("")
+     }
+      setOperand2(operand2 + operand);
+      
+    }
+    console.log(operand1);
+  }
+    const updateOperator = (operator) => {
+      console.log("operand1 " + operand1);
+      setOperator(operator);
+      setIsOperatorOn(true);
+      // isOperatorOn=true;
+    }
 
-     !isOperatorOn?setOperand1(operand1+operand):setOperand2(operand);
-     console.log(operand1);
-  
+
+    return (
+      <>
+        <Button variant="primary" className='mt-5' onClick={handleShow}>
+          Launch Calculator
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Calculator</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Control
+                  type="number"
+                  placeholder="Enter Digits"
+                  autoFocus
+                  value={calcField}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="operandsAndOperators"
+              >
+                <Row className='g-0'>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand(7) }}>7</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand(8) }}>8</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("9") }}>9</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="operands" onClick={() => { updateOperator("division") }}>/</Button>
+                  </Col>
+                </Row>
+                <Row className='g-0 my-3'>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("4") }}>4</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("5") }}>5</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("6") }}>6</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="operands" onClick={() => { updateOperator("addition") }}>+</Button>
+                  </Col>
+                </Row>
+                <Row className='g-0'>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("1") }}>1</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("2") }}>2</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => { updateOperand("3") }}>3</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="operands" onClick={() => { updateOperator("subtraction") }}>-</Button>
+                  </Col>
+                </Row>
+                <Row className='g-0 my-3'>
+                  <Col xs={3}>
+                    <Button id="digits">.</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits">%</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="digits" onClick={() => reset(0)}>CE</Button>
+                  </Col>
+                  <Col xs={3}>
+                    <Button id="operands" onClick={() => { updateOperator("multiplication") }}>*</Button>
+                  </Col>
+                </Row>
+
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" id='operands' onClick={() => onCalculate(operator)}>
+              =
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
   }
 
-  const updateOperator=(operatorArg)=>{
-    console.log("operand1 " + operand1);
-    setOperator(operator);
-    setIsOperatorOn(true);
-    // isOperatorOn=true;
-  }
-
-  
-  return (
-    <>
-      <Button variant="primary" className='mt-5' onClick={handleShow}>
-        Launch Calculator
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Calculator</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control
-                type="number"
-                placeholder="Enter Digits"
-                autoFocus
-                value={calcField}
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="operandsAndOperators"
-            >
-              <Row className='g-0'>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand(7)}}>7</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand(8)}}>8</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("9")}}>9</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="operands" onClick={()=>{updateOperator("division")}}>/</Button>
-                </Col>
-              </Row>
-              <Row className='g-0 my-3'>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("4")}}>4</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("5")}}>5</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("6")}}>6</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="operands" onClick={()=>{updateOperator("addition")}}>+</Button>
-                </Col>
-              </Row>
-              <Row className='g-0'>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("1")}}>1</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("2")}}>2</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={()=>{updateOperand("3")}}>3</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="operands" onClick={()=>{updateOperator("subtraction")}}>-</Button>
-                </Col>
-              </Row>
-              <Row className='g-0 my-3'>
-                <Col xs={3}>
-                  <Button id="digits">.</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits">%</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="digits" onClick={() => reset(0)}>CE</Button>
-                </Col>
-                <Col xs={3}>
-                  <Button id="operands" onClick={()=>{updateOperator("multiplication")}}>*</Button>
-                </Col>
-              </Row>
-
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" id='operands' onClick={() => onCalculate(operator)}>
-            =
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
-
-export default Example;
+  export default Example;
